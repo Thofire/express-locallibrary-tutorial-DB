8c2a0f11caa1b7c2642f5dfc6d345d7cf87892a7
Thofire <46273054+Thofire@users.noreply.github.com>
2021-05-19 21:46:44 -0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.use(helmet());
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Thofire:admin@cs351.9yy2m.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Thofire:admin@cs351.9yy2m.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
